89ee068dbb8a134bf75351a2b013ea86d2b01404
usri.rachid <usri.rachid@gmail.com>
2024-04-21 18:36:33 +0100

    testing

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-document.getElementById("test").innerHTML = "Hello world !";
+let menu = document.querySelector("#menu-bars");
+let header = document.querySelector("header");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-xmark");
+  header.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("fa-xmark");
+  header.classList.remove("active");
+};
